cc96401200fa9b22d1a1151a3653f2dab1c52a20
Renato Menegasso <renato.menegasso@bidu.com.br>
2016-06-22 14:14:56 -0300

    Fix wrong module name

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WebScraper
+module SiteWarmer
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
