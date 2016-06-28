93e8274b57cf9f34448447ddfea5ea01a2661d54
Renato Menegasso <renato.menegasso@bidu.com.br>
2016-06-28 15:47:08 -0300

    Enable recursive execution

--- app/jobs/cache_warmer_job.rb
+++ app/jobs/cache_warmer_job.rb
@@ -1,8 +1,13 @@
 class CacheWarmerJob < ActiveJob::Base
   queue_as :default
 
-  def perform(url)
+  def perform(url, recursive=false)
     warmer = CacheWarmer.new
     result = warmer.warm(url)
+    result.save
+
+    if recursive
+      CacheWarmerJob.set(wait: 50.minutes).perform_later(url, true)
+    end
   end
 end
